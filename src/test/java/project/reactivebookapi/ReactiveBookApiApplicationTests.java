69b6a6dd3d442f392ae9327451443289adb61699
devvinicius <devviniciusalmeida@gmail.com>
2021-12-08 14:16:15 -0300

    Cria o projeto

---
+++ src/test/java/project/reactivebookapi/ReactiveBookApiApplicationTests.java
@@ -0,0 +1,13 @@
+package project.reactivebookapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveBookApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
